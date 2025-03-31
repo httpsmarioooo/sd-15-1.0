de1aa3921643c6c633193327dcd5c530721d14d9
Carlos Mario <carlosmariobig14@gmail.com>
2025-03-31 11:12:19 -0500

    task5

--- task5.js
+++ task5.js
@@ -1,3 +1,15 @@
-export function rubricPassFail() {
-
+export function rubricPassFail(puntuacion) {
+    if (puntuacion >= 5  && puntuacion <= 11){
+        return "Pass";
+    } else if (puntuacion <5){
+        return "Fail"
+    }
 }
+
+console.log(rubricPassFail(6));
+console.log(rubricPassFail(2));
+console.log(rubricPassFail(1));
+console.log(rubricPassFail(11));
+console.log(rubricPassFail(12));
+
+
